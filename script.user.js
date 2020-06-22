0ac660c5e0e10c9f0f99fa0b230e86038f8475aa
tampermonkey-js <67296318+tampermonkey-js@users.noreply.github.com>
2020-06-23 01:39:32 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,6 +4,6 @@
 // @match        https://florr.io/*
 // @run-at       document-start
 // @require      https://code.jquery.com/jquery-latest.min.js
-// @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/library
+// @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/library.js
 // @grant        none
 // ==/UserScript==
