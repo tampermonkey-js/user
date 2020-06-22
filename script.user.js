8ff319bea3847d4cd934f8d3a5620af31f0d7c0d
tampermonkey-js <67296318+tampermonkey-js@users.noreply.github.com>
2020-06-23 01:34:13 +0200

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Floor Speed Switch Petals + Widest Visibility Area + Unlock All Gallery and Mobs and more...
+// @version      0.1
+// @match        https://florr.io/*
+// @run-at       document-start
+// @require      https://code.jquery.com/jquery-latest.min.js
+// @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/library
+// @grant        none
+// ==/UserScript==
