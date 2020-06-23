dda5f5750ea6fc79420a7139549d71fe3d712bf4
tampermonkey-js <67296318+tampermonkey-js@users.noreply.github.com>
2020-06-23 17:55:52 +0200

    Create script1.user.js

---
+++ script1.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Floor.io Unlock All Gallery and Mobs
+// @version      1
+// @author       woold#2808
+// @match        https://florr.io/*
+// @run-at       document-start
+// @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/script1.js
+// ==/UserScript==
