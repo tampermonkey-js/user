10c765c44bb103025299536ec744e88409bae58b
tampermonkey-js <67296318+tampermonkey-js@users.noreply.github.com>
2020-06-24 01:19:47 +0200

    Create script2.user.js

---
+++ script2.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Floor Trash Last Petal & Auto Trasher
+// @version      1
+// @author       woold#2808
+// @match        https://florr.io/*
+// @run-at       document-start
+// @grant		     GM_addStyle
+// @require      https://code.jquery.com/jquery-latest.min.js
+// @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/script2.js
+// ==/UserScript==
