891cf8b5b9f30bc53b7f2fa1b7507f0834b487d1
tampermonkey-js <67296318+tampermonkey-js@users.noreply.github.com>
2020-06-24 02:06:19 +0200

    Update script3.user.js

--- script3.user.js
+++ script3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Florr.io Transfer Your Current Party To Another Computer!
 // @version      1
-// @description  This script allows you to transfer your current party to another computer using your custom password!
+// @description  This script allows you to transfer your current party to another computer using your custom password! When you want to send your account to another computer Press [K] and follow the instruction, and when you want to receive an account from another computer Press [L] and follow the instruction
 // @require      https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js
 // @require      https://raw.githubusercontent.com/tampermonkey-js/user/master/script3.js
 // @author       woold#2808
